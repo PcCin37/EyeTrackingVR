c10e3de7eb9949544f9d2e15e3c4fccc594c9864
Unicornslife <969064199@qq.com>
2024-04-09 23:00:17 +0800

    update with api&&UI

--- Assets/Scripts/newTxtCrtl.cs
+++ Assets/Scripts/newTxtCrtl.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using TMPro;
+using System;
+using System.Globalization;
 
 public class newTxtCrtl : MonoBehaviour
 {
@@ -27,60 +30,153 @@ public class newTxtCrtl : MonoBehaviour
     public TMP_Text fifthWeather;
     public TMP_Text fifthHighest;
 
+    public static Temperature Temperature;
+    public static event Action OnTemperatureLoaded;
 
+    void Start()
+    {
+        string chineseWeekday = "星期二";
+        Debug.Log(WeekdayConverter.ConvertToEnglish(chineseWeekday));
+        StartCoroutine(GetData());
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // firstDate.text = "09-12";
-        // firstWeather.text = "Cloudy";
-        // firstHighest.text = "Highest 76°F";
-        // firstLowest.text = "Lowest 57°F";
+        if (Temperature != null)
+        {
+            firstDate.text = DateFormatConverter.ConvertDate(Temperature.data.forecast[0].ymd);
+            firstDay.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[0].week);
+            firstWeather.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[0].type);
+            firstHighest.text = ExtractTemperature(Temperature.data.forecast[0].high);
 
-        // secondDate.text = "09-13";
-        // secondWeather.text = "Rainy";
-        // secondHighest.text = "Highest 67°F";
-        // secondLowest.text = "Lowest 55°F";
-
-        // thirdDate.text = "09-14";
-        // thirdWeather.text = "Sunny";
-        // thirdHighest.text = "Highest 63°F";
-        // thirdLowest.text = "Lowest 52°F";
-
-        // forthDate.text = "09-15";
-        // forthWeather.text = "Sunny";
-        // forthHighest.text = "Highest 66°F";
-        // forthLowest.text = "Lowest 48°F";
-
-        // fifthDate.text = "09-16";
-        // fifthWeather.text = "Cloudy";
-        // fifthHighest.text = "Highest 69°F";
-        // fifthLowest.text = "Lowest 50°F";
+            secondDate.text = DateFormatConverter.ConvertDate(Temperature.data.forecast[1].ymd);
+            secondDay.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[1].week);
+            secondWeather.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[1].type);
+            secondHighest.text = ExtractTemperature(Temperature.data.forecast[1].high);
 
 
-        firstDate.text = "Mar.25.2024";
-        firstDay.text = "Monday";
-        firstWeather.text = "Angled Rain";
-        firstHighest.text = "33°";
+            thirdDate.text = DateFormatConverter.ConvertDate(Temperature.data.forecast[2].ymd);
+            thirdDay.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[2].week);
+            thirdWeather.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[2].type);
+            thirdHighest.text = ExtractTemperature(Temperature.data.forecast[2].high);
 
-        secondDate.text = "Mar.26.2024";
-        secondDay.text = "Tuesday";
-        secondWeather.text = "Rain";
-        secondHighest.text = "34°";
+            forthDate.text = DateFormatConverter.ConvertDate(Temperature.data.forecast[3].ymd);
+            forthDay.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[3].week);
+            forthWeather.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[3].type);
+            forthHighest.text = ExtractTemperature(Temperature.data.forecast[3].high);
 
-        thirdDate.text = "Mar.27.2024";
-        thirdDay.text = "Wednesday";
-        thirdWeather.text = "Sun Rain";
-        thirdHighest.text = "32°";
+            fifthDate.text = DateFormatConverter.ConvertDate(Temperature.data.forecast[4].ymd);
+            fifthDay.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[4].week);
+            fifthWeather.text = WeekdayConverter.ConvertToEnglish(Temperature.data.forecast[4].type);
+            fifthHighest.text = ExtractTemperature(Temperature.data.forecast[4].high);
 
-        forthDate.text = "Mar.28.2024";
-        forthDay.text = "Thursday";
-        forthWeather.text = "Tornado";
-        forthHighest.text = "30°";
+        }
 
-        fifthDate.text = "Mar.29.2024";
-        fifthDay.text = "Friday";
-        fifthWeather.text = "Windy";
-        fifthHighest.text = "32°";
+
     }
+
+    private IEnumerator GetData()
+    {
+
+        UnityWebRequest unityWebRequest = UnityWebRequest.Get("http://t.weather.sojson.com/api/weather/city/101210101");
+        yield return unityWebRequest.SendWebRequest();
+        if (unityWebRequest.isDone)
+        {
+            Debug.Log(unityWebRequest.downloadHandler.text);
+            Temperature = JsonUtility.FromJson<Temperature>(unityWebRequest.downloadHandler.text);
+            Debug.Log(Temperature.cityInfo.city);
+            OnTemperatureLoaded?.Invoke();
+        }
+    }
+
+    public string ExtractTemperature(string highTemperature)
+    {
+        string[] parts = highTemperature.Split(' ');
+
+        if (parts.Length > 1)
+        {
+            string temperatureWithC = parts[1];
+            if (temperatureWithC.EndsWith("℃"))
+            {
+                return temperatureWithC.Substring(0, temperatureWithC.Length - 1);
+            }
+            else
+            {
+                return temperatureWithC;
+            }
+        }
+        else
+        {
+            return "format error";
+        }
+    }
+
+
+
+    public class DateFormatConverter
+    {
+        public static string ConvertDate(string inputDate)
+        {
+            if (DateTime.TryParseExact(inputDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                return parsedDate.ToString("MMM.dd.yyyy", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return "Invalid date format";
+            }
+        }
+    }
+
+}
+
+
+
+[System.Serializable]
+public class Temperature
+{
+    public string message;
+    public int status;
+    public string date;
+    public string time;
+    public CityInfo cityInfo;
+    public Data data;
+}
+
+[System.Serializable]
+public class CityInfo
+{
+    public string city;
+    public string citykey;
+    public string parent;
+    public string updateTime;
+}
+[System.Serializable]
+public class Data
+{
+    public string shidu;
+    public float pm25;
+    public float pm10;
+    public string quality;
+    public string Temperature;
+    public string ganmao;
+    public List<Forecast> forecast;
+    public Forecast yesterday;
+}
+[System.Serializable]
+public class Forecast
+{
+    public string date;
+    public string high;
+    public string low;
+    public string ymd;
+    public string week;
+    public string sunrise;
+    public string sunset;
+    public int aqi;
+    public string fx;
+    public string fl;
+    public string type;
+    public string notice;
 }
